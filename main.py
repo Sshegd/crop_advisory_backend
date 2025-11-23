8a203a19d843420a9165bee7cd56cd7f9bb6d0e0
Sshegd <153484187+Sshegd@users.noreply.github.com>
2025-11-23 10:26:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -66,5 +66,13 @@ async def home():
         "message": "API is online 🚀"
     }
 
+import uvicorn
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))   # default for local
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+
+
 
 
